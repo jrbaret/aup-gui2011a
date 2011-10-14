48178fd00cf9a946822f333bf85185264a8d9152
Jestoni R. Baret <jrb@csserver2.(none)>
2011-10-14 15:23:10 +0800

    InitializedRepository

--- pyqt/firstprogram/window.py/final.py
+++ pyqt/firstprogram/window.py/final.py
@@ -7,8 +7,8 @@ from PyQt4 import QtGui , QtCore
 class Example ( QtGui . QMainWindow ):
 	def __init__ ( self ):
 		super (  Example , self ). __init__ ()
-		self . setGeometry (500 , 500 , 350 , 350)
-		self . setWindowTitle ( " Grade Calcutaor chorvalu " )
+		self . setGeometry (350 , 350 , 350 , 350)
+		self . setWindowTitle ( " Final " )
 		
 	
 		self . initData ()
@@ -42,43 +42,17 @@ class Example ( QtGui . QMainWindow ):
 		self.edit = QtGui . QPushButton ( " Edit " , self )
 		self.edit . move (240 , 100)
 
-		self.save = QtGui . QPushButton ( 'Save' , self )
-		self.save . move (240 , 140)
-
 		self . lv = QtGui . QListView ( self )
 		self . lv . setModel ( self . model )
 		self . lv . setGeometry (20 , 20 , 200 , 170)
-             
-		label2 = QtGui.QLabel('Grading System', self)
-		label2.move(20, 190)
-		
-		grades = QtGui.QLabel('98-100	A', self)
-		grades.move(20, 210)
-		grades = QtGui.QLabel('95-97	A-', self)
-		grades.move(20, 230)
-		grades = QtGui.QLabel('92-94	B+', self)
-		grades.move(20, 250)
-		grades = QtGui.QLabel('89-91	B', self)
-		grades.move(20, 270)
-		grades = QtGui.QLabel('86-88	B-', self)
-		grades.move(20, 290)
-		grades = QtGui.QLabel('83-85	C+', self)
-		grades.move(20, 310)
-		grades = QtGui.QLabel('80-82	C', self)
-		grades.move(150, 210)		
-		grades = QtGui.QLabel('77-79	C-', self)
-		grades.move(150, 230)
-		grades = QtGui.QLabel('75-76	D', self)
-		grades.move(150, 250)
-		grades = QtGui.QLabel('BELOW	F', self)
-		grades.move(150, 270)		
+		self . lv . move (10 , 150)
 
 	def showDialog(self):
 		text, ok = QtGui.QInputDialog.getText(self, 'ADD', 
             'Enter Information:')
         
 		if ok:
-			self.label.setText(str(text))		
+			names.append.setText(str(text))		
 		
 app = QtGui . QApplication ([])
 ex =  Example ()
--- pyqt/firstprogram/window.py/prefinal.py
+++ pyqt/firstprogram/window.py/prefinal.py
@@ -1,36 +1,72 @@
+#!/usr/bin/python
+
+# QListView.py
+
 from PyQt4 import QtGui , QtCore
-class Example ( QtGui . QWidget ):
+
+class Example ( QtGui . QMainWindow ):
 	def __init__ ( self ):
-
-		super ( Example , self ). __init__ ()
-
-		self . setGeometry (300 , 300 , 250 , 200)
-		self . setWindowTitle ( " QListView " )
-
+		super (  Example , self ). __init__ ()
+		self . setGeometry (350 , 350 , 350 , 350)
+		self . setWindowTitle ( " Name List " )
+		
+	
 		self . initData ()
 		self . initUI ()
-
-	def initData ( self ):
-
-		names = QtCore . QStringList ()
-		names . append ( " Jhayc " )
-		names . append ( " Haycj " )
-		names . append ( " Ayhj " )
-		names . append ( " Yhja " )
-		names . append ( " Jane " )
 	
-		self . model = QtGui . QStringListModel ( names )
+	def initData ( self ):
+		data = ("" )
+		self.model = QtGui.QStandardItemModel (12 , 2)
+
+
+	
 
 	def initUI ( self ):
-		lv = QtGui . QListView ( self )
-		lv . setModel ( self . model )
+		self.tv=QtGui.QTableView(self)
+		self.tv.setModel(self.model)
 
-		layout = QtGui . QVBoxLayout ()
-		layout . addWidget ( lv )
-		self . setLayout ( layout )
+		self.tv.move(20,80)
+		self.tv.resize(205,150)
+		vbox=QtGui.QVBoxLayout()
+		vbox.addWidget(self.tv)
+		
+		self.add = QtGui . QPushButton ( " Add " , self )
+		self.add.setFocusPolicy(QtCore.Qt.NoFocus)
+		self.add . move (240 , 20)
+		self.connect(self.add, QtCore.SIGNAL('clicked()'), 
+            self.showDialog)
+		self.setFocus()
+		
+		self.delete = QtGui . QPushButton ( " Delete " , self )
+		self.delete . move (240 , 60)
+		
+		self.edit = QtGui . QPushButton ( " Edit " , self )
+		self.edit . move (240 , 100)
 
 
+		self . lv = QtGui . QListView ( self )
+		self . lv . setModel ( self . model )
+		self . lv . setGeometry (20 , 20 , 200 , 170)
+             
+		
+
+	def showDialog(self):
+		text, ok = QtGui.QInputDialog.getText(self, 'ADD', 
+            'Enter Information:')
+
+		if ok:
+			self.model.setText(str(text))		
+
+	def add(self,name): 
+		data=str(name)
+		col=0
+		if data != ("") :
+			item = QtGui.QStandardItem(str((data)))
+			self.model.setItem(self.row,col,item )
+			self.row = self.row + 1
+			
 app = QtGui . QApplication ([])
-ex = Example ()
-ex . show ()
+ex =  Example ()
+ex.show ()
 app . exec_ ()
+
